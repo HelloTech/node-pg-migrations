2560f02e3914a26e941e23e819497b3c163433e4
Paulo Diniz <pdiniz_100@yahoo.com>
2019-04-26 11:21:17 -0700

    migration changes

--- bin/migration.js
+++ bin/migration.js
@@ -1,36 +1,49 @@
 #! /usr/bin/env node
+const pg = require('pg');
+const configs = require('../lib/configs');
+const migrate = require('../lib/migrate');
+const generate = require("../lib/generateProcedureMigrationFiles.js");
+const init = require("../lib/initialMigrations.js");
+const add = require("../lib/addMigrationFile");
+const addNotifyTrigger = require("../lib/addNotifyTrigger");
+
+const execRun = function(argv){
+    const pool = new pg.Pool(configs.connectionOptions);
+    migrate.run(argv.directoryName, pool, argv.clear).then(function(){
+        console.log('done');
+    }).catch(function(err){
+        console.log('err: ', err);
+    }).finally(function(){
+        pool.end();
+    });
+};
+
+const execAdd = function(argv) {
+    add(argv.name);
+};
+
+const execAddNotifyTrigger = function(argv){
+    addNotifyTrigger(argv.tableName, argv.triggerName);
+};
+
+
 const args = require('yargs')
     .usage('Usage: $0 <command> [options]')
-    .command('run', 'Run pending migrations', () => {}, (argv) => {
-        const pg = require('pg');
-        const configs = require('../lib/configs');
-        const migrate = require('../lib/migrate');
-        const pool = new pg.Pool(configs.connectionOptions);
-        migrate.run('./migrations', pool, argv.clear).then(function(){
-            console.log('done');
-        }).catch(function(err){
-            console.log('err: ', err);
-        }).finally(function(){
-            pool.end();
-        });
-    })
-    .command("init", 'Initial migration generation based on added/modified/deleted functions and stored procedures', () => {}, () => {
-        const init = require("../lib/initialMigrations.js");
-        init();
-    })
-    .command("generate", 'Generate migrations based on added/modified/deleted functions and stored procedures', () => {}, () => {
-        const generate = require("../lib/generateProcedureMigrationFiles.js");
-        generate();
-    })
+    .command('run [directoryName]', 'Run pending migrations', (yargs) => {
+        yargs
+            .positional('directoryName', {
+                describe: 'The migrations directory',
+                default: './migrations'
+            })
+    }, execRun)
+    .command("init", 'Initial migration generation based on added/modified/deleted functions and stored procedures', () => {}, init)
+    .command("generate", 'Generate migrations based on added/modified/deleted functions and stored procedures', () => {}, generate)
     .command('add <name>', 'Add new migration', (yargs) => {
         yargs
             .positional('name', {
                 describe: 'Name of the new migration'
             })
-    }, (argv) => {
-        const add = require("../lib/addMigrationFile");
-        add(argv.name);
-    })
+    }, execAdd)
     .command('add_notify_trigger <tableName> [triggerName]', 'Add notify trigger', (yargs) => {
         yargs
             .positional('tableName', {
@@ -39,10 +52,7 @@ const args = require('yargs')
             .positional('triggerName', {
                 describe: 'Name of the trigger'
             })
-    }, (argv) => {
-        const addNotifyTrigger = require("../lib/addNotifyTrigger");
-        addNotifyTrigger(argv.tableName, argv.triggerName);
-    })
+    }, execAddNotifyTrigger)
     .demandCommand(1, "Must provide a valid command")
     .alias('c', 'clear')
     .boolean('c')
